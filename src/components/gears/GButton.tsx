458a420db4e43273b83efec50f911a34e55fa0c8
JM Fantin <jmfantin@outlook.com>
2022-07-01 15:45:00 -0300

    improve login page

--- src/components/gears/GButton.tsx
+++ src/components/gears/GButton.tsx
@@ -1,13 +1,15 @@
+import { GoogleLogo } from "phosphor-react";
+
 export function GButton() {
   return (
     <a
       href=""
       className={
-        "group bg-white hover:bg-slate-100 transition-colors text-sm py-2 px-1 flex flex-row items-center rounded-full max-h-12 font-bold uppercase"
+        "group bg-white hover:bg-slate-100 text-blue-600 transition-colors text-sm py-2 px-1  rounded-full max-h-12 font-bold uppercase"
       }
     >
-      <img className="max-h-10 max-w-10" src="/src/assets/google.png" alt="" />
-      <p className="mx-2 text-blue-600 group-hover:text-green-500 transition-colors">
+      <p className="flex flex-row items-center ml-2 mr-3 group-hover:text-green-500 transition-colors">
+        <GoogleLogo size={24} weight="bold" className="mr-2" />
         Entre com Google
       </p>
       {/*consume token for logout*/}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,26 +2,23 @@ import { GButton } from "../components/gears/GButton";
 
 export function Login() {
   return (
-    <div className="min-h-screen bg-blur opacity-90 bg-cover bg-no-repeat flex flex-col items-center">
+    <div className="min-h-screen bg-blur opacity-90 bg-cover bg-no-repeat flex flex-col items-center ">
       {/* call to action and sub box */}
-      <div className="w-full max-w-[1100px] flex items-center justify-center mt-20 mx-auto">
+      <div className="w-full flex items-center justify-center mt-20">
         {/*call to action*/}
         <div className="flex flex-col">
-          <div className="flex flex-row items-center">
-            <h1 className="ml-3 mt-8 text-[2.5rem] leading-tight">
-              <strong className="text-white">
-                super
-                <br />
-                calculadora
-              </strong>
+          <div className="flex flex-row items-center justify-center">
+            <h1 className="mt-8 text-[2.5rem]">
+              <strong className="text-white">super calculadora</strong>
             </h1>
-            <div className="flex-1 flex justify-center mt-8">
-              <GButton />
-            </div>
           </div>
-          <p className="ml-3 mt-4 min-w-[500px] text-lg text-white leading-relaxed">
+          <p className="text-lg text-white leading-relaxed">
             Precificação não precisa ser um estresse.
           </p>
+
+          <div className="flex-1 flex justify-center mt-8">
+            <GButton />
+          </div>
         </div>
       </div>
     </div>
